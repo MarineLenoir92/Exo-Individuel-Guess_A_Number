445d3d3c2c3cfd5d9fd303dfe64a74f804643707
Marine <marine.lenoir@hotmail.fr>
2022-02-20 12:53:45 +0100

    Etape 1 à 6 sans input

--- Script.js
+++ Script.js
@@ -1,33 +1,62 @@
+let iteration = 1
+let minRange = 0
+let maxRange = 50
+
+
+function numberToGuess(){
+    let goodNumber = 0
+    do {
+    goodNumber = parseInt(prompt("Joueur 1 : Saisissez un nombre à deviner entre 0 et 50", "0 < ? < 50"))
+}
+while (goodNumber < 0 || goodNumber > 50)
+    console.log(goodNumber)
+    return goodNumber
+}
+
+
 function question() {
-    let givenNumber = prompt("Choisissez un nombre entre 0 et 50")
-    givenNumber = parseInt(givenNumber)
+    let givenNumber = 0
+    do {
+        givenNumber = parseInt(prompt("Joueur 2: Ceci est votre tentative n°  " + iteration + " Choisissez un nombre entre " + minRange + " et " + maxRange, minRange + " < ? < " + maxRange))
+    } while (givenNumber < minRange || givenNumber > maxRange)// "||" signifie "ou"
+    console.log(givenNumber)
+    iteration ++
     return givenNumber
 }
 
-function didIwin(number) {
-    if (number < 22) {
-        return false
-    } else if (number > 22) {
-        return false
-    } else if (number == 22) {
-        document.write("Vrai: Bravo vous avez trouvé le nombre")
+
+function didIwin(number, choiceNumber) {
+
+    if (number == choiceNumber) {
+        alert("Vrai: Bravo vous avez trouvé le nombre")
         return true
+        }
+
+    else if (number < choiceNumber) {
+        if (number > minRange){
+            minRange = number
+        }
+        alert("Plus grand")
+        
+
+    } else {
+        if (number < maxRange){
+            maxRange = number
+        }
+        alert("Plus petit")  
     }
+
+    return false
+  
 }
 
 
 function gamePlay() {
-    //Déclare variable resultfinal en dépôt global afin qu'elle soit utilisable en dépot local dans boucle "do" //
-let resultFinal = false
-    do {
-        let resultat = question()
-        resultFinal = didIwin(resultat)  
-        //Equivaut à didIwin(resultat) //
-        // + let resultFinal = didIwin()//
-         console.log(resultFinal) 
+    let choiceNumber = numberToGuess () // retour de goodNumber, chiffre à deviner entre 0 et 50 
+    let resultat = question() // retour de givenNumber, chiffre choisi entre 0 et 50
+    while (!didIwin(resultat, choiceNumber)){
+        resultat = question()
     }
-
-    while(resultFinal == false )
 }
 
 
